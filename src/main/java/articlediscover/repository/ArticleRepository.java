e60f37b240a54ded36112efaf019b68531d90da2
Ahmed_Ubuntu <qarfaoui@gmail.com>
2024-11-24 11:23:57 +0100

    Refactoring and disconnect from solide lib

---
+++ src/main/java/articlediscover/repository/ArticleRepository.java
@@ -0,0 +1,12 @@
+package articlediscover.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import articlediscover.model.Article;
+
+public interface ArticleRepository extends JpaRepository<Article, Integer> {
+	Optional<Article> findById(Integer id);
+
+}
